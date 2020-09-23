e8ecb58d7a6d21a886e59f721c45c4e657c2f307
Andrey Smirnov <andrey.smirnov@team.wrike.com>
2020-09-24 02:06:32 +0300

    models consumer application just to demonstrate an approach to import code

--- lesson_01/cocktail_app_models_consumer/lib/main.dart
+++ lesson_01/cocktail_app_models_consumer/lib/main.dart
@@ -1,12 +1,22 @@
 import 'package:cocktailappmodels/models.dart';
-import 'package:flutter/material.dart';
-
 import 'package:cocktailappmodels/src/repository/sync_cocktail_repository.dart';
+import 'package:flutter/material.dart';
 
 void main() {
   final repository = SyncCocktailRepository();
   final cocktails = repository.fetchAllCocktails();
-  runApp(Center(
-    child: Text('We have ${cocktails.length} cocktails at the moment!'),
-  ));
+  final popularCocktails = repository.fetchPopularCocktails();
+
+  runApp(
+    MaterialApp(
+      home: Container(
+        color: Colors.white,
+        alignment: Alignment.center,
+        child: Text(
+          'We have ${cocktails.length} cocktails '
+          'and ${popularCocktails.length}  popular at the moment!',
+        ),
+      ),
+    ),
+  );
 }
