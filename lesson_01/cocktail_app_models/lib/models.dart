f78e780a7cea3359d782eef1b609fe8a5a39ba2a
Andrey Smirnov <andrey.smirnov@team.wrike.com>
2020-09-24 01:15:53 +0300

    async and sync repositories

--- lesson_01/cocktail_app_models/lib/models.dart
+++ lesson_01/cocktail_app_models/lib/models.dart
@@ -4,4 +4,4 @@ export 'src/model/cocktail_type.dart';
 export 'src/model/glass_type.dart';
 export 'src/model/ingredient.dart';
 export 'src/model/ingredient_definition.dart';
-export 'src/repository/cocktail_repository.dart';
+export 'src/repository/sync_cocktail_repository.dart';
--- lesson_04/cocktail_db_http_using/lib/models.dart
+++ lesson_04/cocktail_db_http_using/lib/models.dart
@@ -1,5 +1,6 @@
 export 'src/model/cocktail.dart';
 export 'src/model/cocktail_category.dart';
+export 'src/model/cocktail_definition.dart';
 export 'src/model/cocktail_type.dart';
 export 'src/model/glass_type.dart';
 export 'src/model/ingredient.dart';
