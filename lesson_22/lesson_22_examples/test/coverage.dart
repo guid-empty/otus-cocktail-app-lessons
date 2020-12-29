12344b1ace9d141a8afe9b39aacd466b9e1e4e3f
Maria Smirnova <marrrusya@yandex-team.ru>
2020-12-29 04:38:01 +0300

    initial

---
+++ lesson_22/lesson_22_examples/test/coverage.dart
@@ -0,0 +1,9 @@
+@TestOn('vm')
+import 'ui/pages/details/cocktail_description/00_cocktail_title_test.dart' as t0;
+import 'ui/pages/details/cocktail_description/01_cocktail_title_test_by_semantics.dart' as t1;
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  t0.main();
+  t1.main();
+}
